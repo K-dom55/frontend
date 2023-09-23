f99854fc028db5041359575addfa263bbe6ae4cd
dolphinlmg <dolphinlmg@naver.com>
2023-09-23 15:25:11 +0900

    fix: theme 관련 오류 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,7 +12,7 @@ export const theme = {
   gray100: '#f3f3f3',
   gray50: '#f9f9f9',
   primaryBlack: '#1d1d1d',
-  secondaryNeonGreen: '#6568eb',
+  secondaryNeonGreen: '#ecfe54',
   background: '#f9f9f9',
   aggro: css`
     font-family: 'SBAggroB';
@@ -20,51 +20,51 @@ export const theme = {
   headline1: css`
     font-size: 24px;
     font-weight: 700;
-    line-height: 150%;
+    line-height: 140%;
   `,
   headline2: css`
     font-size: 22px;
     font-weight: 700;
-    line-height: 150%;
+    line-height: 140%;
   `,
   headline3: css`
     font-size: 20px;
     font-weight: 700;
-    line-height: 150%;
+    line-height: 140%;
   `,
   body1: css`
     font-size: 18px;
     font-weight: 500;
-    line-height: 150%;
+    line-height: 140%;
   `,
   body2: css`
     font-size: 16px;
     font-weight: 500;
-    line-height: 150%;
+    line-height: 140%;
   `,
   body3: css`
     font-size: 14px;
     font-weight: 500;
-    line-height: 150%;
+    line-height: 140%;
   `,
   body4: css`
     font-size: 12px;
     font-weight: 500;
-    line-height: 150%;
+    line-height: 140%;
   `,
   caption1: css`
     font-size: 16px;
     font-weight: 400;
-    line-height: 150%;
+    line-height: 140%;
   `,
   caption2: css`
     font-size: 14px;
     font-weight: 400;
-    line-height: 150%;
+    line-height: 140%;
   `,
   caption3: css`
     font-size: 12px;
     font-weight: 400;
-    line-height: 150%;
+    line-height: 140%;
   `,
 };
