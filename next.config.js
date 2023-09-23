eeed94f1d375f69e2d69559cdd0c641f1c0cd8b1
dolphinlmg <dolphinlmg@naver.com>
2023-09-24 03:41:58 +0900

    chore: youtube 썸네일을 위한 설정 추가

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'kdom.s3.ap-northeast-2.amazonaws.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+      },
     ],
   },
   webpack: (config) => {
