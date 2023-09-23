346f943430efe9d594cdca832abb5353303320d6
dolphinlmg <dolphinlmg@naver.com>
2023-09-23 12:56:23 +0900

    chore: init repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: '../assets/fonts/woff2/PretendardVariable.woff2',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main
+      className={pretendard.className}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'auto',
+        minHeight: '100dvh',
+        maxWidth: '440px',
+        margin: '0 auto',
+        outline: '1px solid black',
+      }}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+import { css } from '@emotion/react';
+
+export const theme = {
+  gray900: '#1d1d1d',
+  gray800: '#3c3c3c',
+  gray700: '#5b5b5b',
+  gray600: '#6e6e6e',
+  gray500: '#979797',
+  gray400: '#b7b7b7',
+  gray300: '#dadada',
+  gray200: '#eaeaea',
+  gray100: '#f3f3f3',
+  gray50: '#f9f9f9',
+  primaryBlack: '#1d1d1d',
+  secondaryNeonGreen: '#6568eb',
+  background: '#f9f9f9',
+  aggro: css`
+    font-family: 'SBAggroB';
+  `,
+  headline1: css`
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 150%;
+  `,
+};
