41f7667f133d30b1290799ca2f964fbcaded075e
dolphinlmg <dolphinlmg@naver.com>
2023-09-24 09:45:40 +0900

    fix: 빌드 안되는 버그 수정

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -1,5 +1,5 @@
 export { default as Button } from './Button';
 export { default as DeemLayer } from './DeemLayer';
-export { default as ShareKakao } from './ShareKakao';
+export { default as ShareKakao } from './Sharekakao';
 export { default as ShareNative } from './ShareNative';
 export { default as ShareTwitter } from './ShareTwitter';
