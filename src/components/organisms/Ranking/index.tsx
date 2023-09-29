8ebc4eccb95e40e63bb428d9091abb8407c0b80c
jxxunnn <roof2586@naver.com>
2023-09-29 22:45:30 +0900

    feat: group ranking components

---
+++ src/components/organisms/Ranking/index.tsx
@@ -0,0 +1,14 @@
+import Header from './Header';
+import Tabs from './Tabs';
+import Chart from './Chart';
+import ListItem from './ListItem';
+
+export default Object.assign(
+  {},
+  {
+    Header,
+    Tabs,
+    Chart,
+    ListItem,
+  },
+);
