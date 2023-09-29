dc8a77d7a0329f4f0b7e4e54c2382a8c41a3b6e1
jxxunnn <roof2586@naver.com>
2023-09-29 22:47:57 +0900

    feat: in the process of developing ranking page

--- src/pages/ranking/index.tsx
+++ src/pages/ranking/index.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Ranking as RankingTemplate } from '@/components/templates';
+import { getArticleList } from '@/api/article';
+import { ArticleListDTO } from '@/api/article';
 
 export default function Ranking() {
-  return <div>Ranking</div>;
+  const [articleList, setArticleList] = useState<ArticleListDTO[]>([]);
+  const [cursorId, setCursorId] = useState<undefined | number>();
+
+  useEffect(() => {
+    getArticleList(cursorId).then((res) => {
+      setArticleList(res);
+    });
+  }, [cursorId]);
+
+  const handleNext = (id: number) => {
+    setCursorId(id);
+  };
+
+  return (
+    <>
+      <RankingTemplate list={articleList} handleNext={handleNext} />
+    </>
+  );
 }
